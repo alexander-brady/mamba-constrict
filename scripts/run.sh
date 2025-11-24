4ac5ecebb1d3597525b475a0a931235c6c281e7a
alexander-brady <bradya@student.ethz.ch>
2025-11-24 22:59:50 +0100

    feat: slurm scripts

---
+++ scripts/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=finetune_mamba
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH --ntasks=1
+#SBATCH --tmp=64G
+#SBATCH --mem-per-cpu=64G
+#SBATCH --gpus-per-node=1
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpumem:64g
+#SBATCH --mail-type=END,FAIL
+
+echo "Beginning finetuning at $(date)"
+
+source scripts/env.sh
+
+uv run -m finetune
+
+echo "Finished finetuning at $(date)"
