35f371a62567441d644a25858f11b7f105bc0011
Ege Zorlutuna <ezorlutuna@ethz.ch>
2025-12-15 16:02:11 +0100

    norms only with the last hidden state

--- finetune/criterion/__init__.py
+++ finetune/criterion/__init__.py
@@ -1,3 +1,15 @@
+from .L1 import L1HiddenStateNorm
+from .L2 import L2HiddenStateNorm
 from .base import Criterion
+from .mahalanobis_energy import MahalanobisEnergy
+from .temporal_drift import TemporalDrift
+from .zero import EmptyCriterion
 
-__all__ = ["Criterion"]
+__all__ = [
+    "Criterion",
+    "EmptyCriterion",
+    "L1HiddenStateNorm",
+    "L2HiddenStateNorm",
+    "MahalanobisEnergy",
+    "TemporalDrift",
+]
