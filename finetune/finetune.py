69a8f7fe3927b9937bfd7b1689cdce88cbf165c6
Alexander Brady <bradya@ethz.com>
2026-01-02 11:14:24 +0100

    feat: move logging and model saving to root node only

--- finetune/finetune.py
+++ finetune/finetune.py
@@ -17,9 +17,14 @@ logger = logging.getLogger(__name__)
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="config")
 def finetune(cfg: DictConfig):
+    # Get local rank for distributed training
+    rank = int(os.environ["SLURM_LOCALID"])
+    logger.info(f"Local rank: {rank}")
+    
     # Log Hydra working directory
     hydra_wd = HydraConfig.get().runtime.output_dir
-    logger.info(f"Hydra working directory: {hydra_wd}")
+    if rank == 0:
+        logger.info(f"Hydra working directory: {hydra_wd}")
 
     # Set seed for reproducibility
     L.seed_everything(cfg.seed)
