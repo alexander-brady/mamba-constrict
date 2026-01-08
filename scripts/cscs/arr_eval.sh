34bc653ffc15ac3f5a7cae82b4f9c117f5390aef
Alexander Brady <129788423+alexander-brady@users.noreply.github.com>
2026-01-08 11:49:52 +0100

    feat: wandb args in arr_eval

--- scripts/cscs/arr_eval.sh
+++ scripts/cscs/arr_eval.sh
@@ -23,11 +23,13 @@ echo "Starting LM-eval of ${MODEL_NAME} at $(date)"
 echo "MODEL_PATH=${MODEL_PATH}"
 
 MODEL_ARGS="pretrained=${MODEL_PATH} tokenizer=state-spaces/mamba-2.8b-hf tensor_parallel_size=4 dtype=bfloat16 trust_remote_code=True max_model_len=32768"
+WANDB_ARGS="project=eval-mamba entity=mamba-monks name=${MODEL_NAME} dir=${PROJECT_DIR}/outputs/lm-eval/"
 
 lm-eval run \
     --config "${PROJECT_DIR}/eval/lm-eval.yaml" \
     --model_args "${MODEL_ARGS}" \
     --output_path "${PROJECT_DIR}/results/lm-eval/${MODEL_NAME}" \
+    --wandb_args "${WANDB_ARGS}" \
     --tasks lambada_openai,hellaswag,piqa,arc_easy,arc_challenge,winogrande,openbookqa
 
 echo "Finished LM-eval evaluation at $(date)"
