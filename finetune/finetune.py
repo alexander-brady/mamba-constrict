1e4278356db5d9b11f0a44ddc398802c7ec9644f
alexander-brady <bradya@student.ethz.ch>
2026-01-08 11:16:58 +0100

    refactor: save model to model_dir/run_id

--- finetune/finetune.py
+++ finetune/finetune.py
@@ -68,15 +68,16 @@ def finetune(cfg: DictConfig):
     # Fine-tuning
     trainer.fit(fine_tuner, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
-    # Build save path: models/model_name_criterion_dataset[_task]
-    model_name = cfg.model.name.split("/")[-1]
-    dataset_name = cfg.data.name.split("/")[-1]
-    criterion_name = cfg.loss._target_.split(".")[-1].lower()
-    if cfg.loss.get("weight", None) is not None:
-        criterion_name += f"_w{cfg.loss.weight}"
-    save_path = f"{cfg.model_dir}/{model_name}_{criterion_name}_{dataset_name}"
-    if cfg.data.get("use_babilong", False) and cfg.data.get("task"):
-        save_path += f"_{cfg.data.task}"
+    # Build save path: models/model_name_criterion[_w<lambda>]_dataset[_task]
+    # model_name = cfg.model.name.split("/")[-1]
+    # dataset_name = cfg.data.name.split("/")[-1]
+    # criterion_name = cfg.loss._target_.split(".")[-1].lower()
+    # if cfg.loss.get("weight", None) is not None:
+    #     criterion_name += f"_w{cfg.loss.weight}"
+    # save_path = f"{cfg.model_dir}/{model_name}_{criterion_name}_{dataset_name}"
+    # if cfg.data.get("use_babilong", False) and cfg.data.get("task"):
+    #     save_path += f"_{cfg.data.task}"
+    save_path = f"{cfg.model_dir}/{cfg.run_id}"
 
     # Save the fine-tuned model
     model.save_pretrained(save_path)
